763a4dc3fe2d26c4023d4073b9737c54cfcf8cf7
Joseph Davidson <j.davidson18@gmail.com>
2025-01-18 07:28:17 -0700

    header updated, outlets working

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,11 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import styles from "./Header.module.css";
 
 
 function Header() {
+
   return (
-    <>
-    HEADER
-    </>
+    <div className={styles["mainContain"]}>
+      <div className={styles["leftHeader"]}>
+        <div className={styles["navMenu"]}>
+          <a>
+            <Link
+              to="/about"
+              style={{ color: "black", textDecoration: "none" }}
+            >
+              <span style={{ marginRight: -9 }}>About Me</span>
+            </Link>
+          </a>
+        </div>
+      </div>
+
+      <div className={styles["centerHeader"]}>
+        <div className={styles["centerHeader2"]}>
+          <div className={styles["navMenu"]}>
+            <a>
+              <Link to="/" style={{ color: "black", textDecoration: "none" }}>
+                <span style={{ fontSize: "larger" }}>
+                  LEARN MUSIC
+                </span>
+              </Link>
+            </a>
+          </div>
+        </div>
+      </div>
+
+      <div className={styles["rightHeader"]}>
+        <div className={styles["navMenu"]}>
+          <a>
+            <Link
+              to="/portfolio"
+              style={{ color: "black", textDecoration: "none" }}
+            >
+              <span>Portfolio</span>
+            </Link>
+          </a>
+        </div>
+      </div>
+    </div>
   );
 }
+
 export default Header;
